3d31d5fc29e1aebe26eb968acfeed971f7024381
Jo Provost <l1kw1d@users.noreply.github.com>
2025-05-16 10:18:02 -0400

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,5 +1,5 @@
 find ~ -name ".bash_history*" -type f | while read -r file; do 
-    grep 's3' "$file" | while read -r line; do 
+    grep -E 's3|mount|gdrive' "$file" | while read -r line; do 
         stat --format="%y %n" "$file" | awk -v start="2025-03-23" -v end="2025-04-05" '
         { 
             if ($1 >= start && $1 <= end) 
