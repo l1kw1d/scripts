5464b255a7037333aedc124236ec4967dcb0245f
Jo Provost <l1kw1d@users.noreply.github.com>
2025-05-16 10:16:05 -0400

    Create s3.sh

---
+++ s3.sh
@@ -0,0 +1,9 @@
+find ~ -name ".bash_history*" -type f | while read -r file; do 
+    grep 's3' "$file" | while read -r line; do 
+        stat --format="%y %n" "$file" | awk -v start="2025-03-23" -v end="2025-04-05" '
+        { 
+            if ($1 >= start && $1 <= end) 
+                print "[" $1 "] " file ": " line 
+        }'
+    done
+done
